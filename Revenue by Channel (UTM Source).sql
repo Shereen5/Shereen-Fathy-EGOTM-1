d50e269b5ebdd560213f62735d5a93135a0a2163
Shereen5 <shereen_fathy@hotmail.co.uk>
2026-02-17 20:17:35 +0000

    Add files via upload

---
+++ Revenue by Channel (UTM Source).sql
@@ -0,0 +1,8 @@
+SELECT
+  website_sessions.utm_source,
+  SUM(orders.price_usd) AS revenue_usd
+FROM `OperationsDb.orders`
+JOIN `OperationsDb.website_sessions` 
+  ON website_sessions.website_session_id = orders.website_session_id
+GROUP BY website_sessions.utm_source
+ORDER BY revenue_usd DESC;
