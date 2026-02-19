65b07ae16a76e03e2a40587b3da9f7ca5417af12
Shereen5 <shereen_fathy@hotmail.co.uk>
2026-02-19 13:00:49 +0000

    Update Revenue by Channel (UTM Source).sql

--- Revenue by Channel (UTM Source).sql
+++ Revenue by Channel (UTM Source).sql
@@ -1,8 +1,9 @@
 SELECT
   website_sessions.utm_source,
   SUM(orders.price_usd) AS revenue_usd
-FROM `OperationsDb.orders`
-JOIN `OperationsDb.website_sessions` 
+FROM OperationsDb.orders
+INNER JOIN OperationsDb.website_sessions
   ON website_sessions.website_session_id = orders.website_session_id
 GROUP BY website_sessions.utm_source
 ORDER BY revenue_usd DESC;
+
